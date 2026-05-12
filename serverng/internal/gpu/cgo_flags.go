baa4692ec9bd4c104236ceef4b429f7e8148b30c
rampage <4042540+gbozo@users.noreply.github.com>
2026-05-12 23:16:28 +0300

    build fix

--- serverng/internal/gpu/cgo_flags.go
+++ serverng/internal/gpu/cgo_flags.go
@@ -1,12 +1,10 @@
-//go:build gpu_vulkan && linux && !musl
+//go:build (gpu_vulkan && linux)
 
 package gpu
 
 /*
 #cgo CFLAGS: -I${SRCDIR}/c -I${SRCDIR}/c/vkFFT -I/usr/include/glslang/Include
-
-// Debian/Ubuntu use static libs (.a) requiring linker groups for circular deps.
-#cgo LDFLAGS: -Wl,--start-group -lglslang -lMachineIndependent -lOSDependent -lHLSL -lOGLCompiler -lGenericCodeGen -lSPVRemapper -lglslang-default-resource-limits -lSPIRV-Tools-opt -lSPIRV-Tools -lSPIRV -lglslang -lpthread -Wl,--end-group -lvulkan -lm -lstdc++
+#cgo linux LDFLAGS: -lglslang -lglslang-default-resource-limits -lSPIRV-Tools-opt -lSPIRV -lvulkan -lm -lstdc++
 
 #include "c/vulkan_probe.c"
 #include "c/vulkan_device.c"
